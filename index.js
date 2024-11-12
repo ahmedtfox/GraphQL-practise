eeff1d7fe8b2b75a42403ce99580ad0b70438c61
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-11-12 11:15:15 +0200

    typeDefs

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import process from "process";
 const PORT = process.env.PORT;
+import { typeDefs } from "./schema";
 // setup server
 
-const server = new ApolloServer({
-    
-});
+const server = new ApolloServer({});
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: PORT },
